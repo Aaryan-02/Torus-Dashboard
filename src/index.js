bcbbb601286144004736a0512d52dc623c7e58b5
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2024-12-14 20:44:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {store} from "./helpers"
 import { Provider } from 'react-redux';
-import App from 'components/App/App.jsx';
+import App from './components/App/App.jsx';
 import "assets/css/material-dashboard-react.css?v=1.5.0";
 
 // setup fake backend
