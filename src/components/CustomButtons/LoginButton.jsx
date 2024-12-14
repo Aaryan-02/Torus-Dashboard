09e78106fd8f93ebb29561feac708b6c253ee656
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2024-12-14 22:52:47 +0530

    Update LoginButton.jsx

--- src/components/CustomButtons/LoginButton.jsx
+++ src/components/CustomButtons/LoginButton.jsx
@@ -10,7 +10,7 @@ import Button from "@material-ui/core/Button";
 
 // core components
 
-import buttonStyle from "assets/jss/material-dashboard-react/components/loginButtonStyle.jsx";
+import buttonStyle from "../../assets/jss/material-dashboard-react/components/loginButtonStyle.jsx";
 
 function RegularButton({ ...props }) {
   const {
