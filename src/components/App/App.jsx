3fb8761c44487d09c82f26cae810528cda2601e6
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2024-12-14 20:53:40 +0530

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { history } from "helpers";
-import * as alertActions  from "modules/Alert/alert"
-import Layout from "layout/Layout/Layout.jsx";
-import  {PrivateRoute}  from "routes";
-import  {Login}  from "components/Login/Login";
+import { history } from "../../helpers";
+import * as alertActions  from "../../modules/Alert/alert";
+import Layout from "../../layout/Layout/Layout.jsx";
+import  {PrivateRoute}  from "../../routes";
+import  {Login}  from "../../components/Login/Login";
 
 class App extends React.Component {
   constructor(props) {
