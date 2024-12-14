05fe354421f727c62bc61005b754c7f19cdcb83d
Aaryan <102778679+Aaryan-02@users.noreply.github.com>
2024-12-14 21:03:50 +0530

    Update Layout.jsx

--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -8,9 +8,9 @@ import "perfect-scrollbar/css/perfect-scrollbar.css";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 // core components
-import Header from "components/Header/Header.jsx";
+import Header from "../../components/Header/Header.jsx";
 // import Footer from "components/Footer/Footer.jsx";
-import Sidebar from "components/Sidebar/Sidebar.jsx";
+import Sidebar from "../../components/Sidebar/Sidebar.jsx";
 
 import DashboardRoutes from "../../routes/DashboardRoute.jsx";
 
